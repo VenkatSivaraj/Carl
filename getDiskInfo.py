24aba75a8a4f0b0dde3bc3bb4bd77e6d8fff9d1c
VenkatSivaraj <venkat.sivaraj@gmail.com>
2015-10-09 23:27:02 -0400

    Added first version of script

---
+++ getDiskInfo.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import sys
+import re
+
+
+if len(sys.argv) == 2:
+	print ("Usage: %s <path to diskbyid output> <path to LSI output>" % sys.argv[0])
+	sys.exit(0)
+
+guid_devid_map=dict()
+guid_encl_slot_map=dict()
+ansi_escape = re.compile(r'\x1b[^m]*m')
+
+def create_guid_dev_id_map(diskByIDFile):
+    """Reads output of diskbyid and maps GUID to device id """
+    wwn_regex = r'wwn-0x(\w+).+$'
+    with open(diskByIDFile) as f:
+        for line in f:
+            match = re.search(wwn_regex, line) 
+            if match != None:
+                guid_devid_map[match.group(1)] = line[-4:]
+
+def create_guid_encl_slot_map(lsiOutputFile):
+    """Reads LSI config output and maps GUID to enclosure and slot number """
+    scan = 0
+    with open(lsiOutputFile) as f:
+        for line in f:
+            if line.find('Device') >= 0:
+                scan = 1
+            if scan and (line.find('Slot #') > 0):
+                slot = line.split(':')[1].strip()
+            if scan and (line.find('GUID') > 0):
+                guid_encl_slot_map[line.split(':')[1].strip()] = slot
+                scan = 0
+
+def print_mapping():
+    for key in guid_encl_slot_map.keys():
+        print("%s --> %s" % (guid_encl_slot_map[key], guid_devid_map[key])) 
+
+
+
+create_guid_dev_id_map(sys.argv[1])
+create_guid_encl_slot_map(sys.argv[2])
+print_mapping()
